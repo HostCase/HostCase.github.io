2ab8b5a96ef186c2e3b05f902bbaf7f4bd179d0f
HostCase <75564349+HostCase@users.noreply.github.com>
2024-12-30 20:21:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let score = 0;
 let level = 1;
 let userId = null;
 
-const SERVER_URL = 'http://176.32.39.168:8000'; // 'http://127.0.0.1:8000';      'http://176.32.39.168:8000';
+const SERVER_URL = 'http://176.32.39.168:8000';
 
 function parseQueryString(queryString) {
     const params = {};
@@ -61,7 +61,7 @@ function getUserData() {
     })
     .catch(error => {
         console.error('Ошибка при запросе /verify:', error);
-        userInfo.innerText = 'Произошла ошибка при верификации. Версия 1.2';
+        userInfo.innerText = 'Произошла ошибка при верификации. Версия 1.3';
     });
 }
 
