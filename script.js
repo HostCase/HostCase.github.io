5f547b1190a12e346ecf8ad56d57ca8f35fb2fb8
HostCase <75564349+HostCase@users.noreply.github.com>
2024-12-30 21:36:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let score = 0;
 let level = 1;
 let userId = null;
 
-const SERVER_URL = 'https://vovasticcoinbot.tech:8000';
+const SERVER_URL = 'https://vovasticcoinbot.tech:443';
 
 function parseQueryString(queryString) {
     const params = {};
